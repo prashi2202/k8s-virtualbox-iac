b9db1abb1cb5032daa20c09209626da128c37cbc
prashi2202 <ds.prashanth@yahoo.co.in>
2026-01-06 14:47:12 +0000

    Fixing the provider

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -3,8 +3,8 @@ terraform {
 
   required_providers {
     virtualbox = {
-      source  = "shekeriev/virtualbox"
-      version = "0.0.4"
+      source  = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
     }
   }
 }
